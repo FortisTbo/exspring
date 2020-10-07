3a147deee14ba65ad6388f31a74896bb4e48c349
FortisTbo <mail4tbo@gmail.com>
2020-10-07 10:44:11 +0200

    ExB1

---
+++ ExB1/src/main/java/be/abis/exercise/ExB1pplication.java
@@ -0,0 +1,13 @@
+package be.abis.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExB1pplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExB1pplication.class, args);
+	}
+
+}
