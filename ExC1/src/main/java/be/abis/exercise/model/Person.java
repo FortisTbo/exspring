c1b0be9bebdb637d09868ae8c05b97f079cb4ad2
FortisTbo <mail4tbo@gmail.com>
2020-10-12 10:51:35 +0200

    ExC1a b c and d

--- ExC1/src/main/java/be/abis/exercise/model/Person.java
+++ ExC1/src/main/java/be/abis/exercise/model/Person.java
@@ -1,45 +1,56 @@
 package be.abis.exercise.model;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Person {
-	
+
 	private int personId;
-	@NotBlank( message="Please enter the name") 
+	@NotBlank(message = "Please enter the name")
 	private String firstName;
 	private String lastName;
+	@Min(value = 18, message = "The person’s age should be 18+")
 	private int age;
-	@NotBlank( message="Please enter the email address") 
+	@NotBlank(message = "Please enter the email address")
+	@Email(message="Please enter a valid email address")
 	private String emailAddress;
-	@NotBlank( message="Please enter the password") 
+	@Size(min = 6, message = "Password should be minimum 6 characters long")
 	private String password;
 	private String language;
 	@Valid
 	private Company company;
-	
-	
+
 	public int getPersonId() {
 		return personId;
 	}
+
 	public void setPersonId(int personId) {
 		this.personId = personId;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
@@ -47,15 +58,19 @@ public class Person {
 	public String getEmailAddress() {
 		return emailAddress;
 	}
+
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getLanguage() {
 		return language;
 	}
@@ -67,15 +82,15 @@ public class Person {
 	public Company getCompany() {
 		return company;
 	}
+
 	public void setCompany(Company company) {
 		this.company = company;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Person with id " + personId + ", " + firstName + " "+ lastName + ", works for " +company.getName() + " in " + company.getAddress().getTown();
+		return "Person with id " + personId + ", " + firstName + " " + lastName + ", works for " + company.getName()
+				+ " in " + company.getAddress().getTown();
 	}
-
-	
 
 }
